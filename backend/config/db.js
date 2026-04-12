1d63333ce1a87c4d98401d2972e43a74e292e22d
sachinkanchan989 <106974514+Sachin989kanchan@users.noreply.github.com>
2026-04-12 23:38:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -20,11 +20,7 @@ const pool = mysql.createPool({
   }
 }); // ← pehle createPool band karo
 
-// ✅ Phir pool.on bahar
-pool.on('connection', (connection) => {
-  connection.query(
-    "SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
-  );
+
 });
 
 module.exports = pool;
